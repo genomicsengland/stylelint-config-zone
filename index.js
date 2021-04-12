3f0414f9b68dc33560eb1788ea8eca9d7b1c4cbe
Peter Hudson <peter.hudson@genomicsengland.co.uk>
2021-04-12 13:14:49 +0100

    NTOS-5556: creating a copy of the style lint

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['if', 'else'],
+      },
+    ],
+    'at-rule-no-unknown': null,
+    'block-closing-brace-newline-after': [
+      'always',
+      {
+        ignoreAtRules: ['if', 'else'],
+      },
+    ],
+    'selector-attribute-quotes': 'always',
+    'string-quotes': 'single',
+  },
+};
---
+++ index.test.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const { name: NPM_PACKAGE } = require('./package.json');
+
+const execAsync = promisify(exec);
+
+describe(NPM_PACKAGE, () => {
+  it('config does not error', async () => {
+    const { stdout } = await execAsync(
+      `./node_modules/.bin/stylelint index.scss --printConfig --config=${require.resolve(
+        './index.js'
+      )}`
+    );
+
+    expect(JSON.parse(stdout)).toMatchSnapshot();
+  });
+});
